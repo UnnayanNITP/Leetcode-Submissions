f1353001921a08b241c98738ee6e6e7639292e71
UnnayanNITP <157901866+UnnayanNITP@users.noreply.github.com>
2024-07-18 00:28:13 +0530

    Time: 40 ms (96.47%) | Memory: 48.9 MB (41.60%) - LeetSync

--- 2732-counter/counter.js
+++ 2732-counter/counter.js
@@ -2,10 +2,10 @@
  * @param {number} n
  * @return {Function} counter
  */
-var createCounter = function(n) {
-    return function() {
+var createCounter = function (n) {
+    return function () {
         return n++;
-    };   
+    };
 
 };
 
