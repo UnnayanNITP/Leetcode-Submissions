6c576fa911ef5139369e48035b184d8af4b71c79
UnnayanNITP <157901866+UnnayanNITP@users.noreply.github.com>
2024-07-31 23:29:13 +0530

    Time: 119 ms (8.95%) | Memory: 67 MB (12.47%) - LeetSync

---
+++ 2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.cpp
@@ -0,0 +1,70 @@
+
+
+class Solution {
+public:
+    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
+        vector<int> rowOrder = topologicalSort(k, rowConditions);
+        vector<int> colOrder = topologicalSort(k, colConditions);
+        
+        if (rowOrder.empty() || colOrder.empty()) {
+            return {};
+        }
+        
+        vector<vector<int>> matrix(k, vector<int>(k, 0));
+        unordered_map<int, int> rowPosition;
+        unordered_map<int, int> colPosition;
+        
+        for (int i = 0; i < k; ++i) {
+            rowPosition[rowOrder[i]] = i;
+            colPosition[colOrder[i]] = i;
+        }
+        
+        for (int i = 1; i <= k; ++i) {
+            matrix[rowPosition[i]][colPosition[i]] = i;
+        }
+        
+        return matrix;
+    }
+    
+private:
+    vector<int> topologicalSort(int k, vector<vector<int>>& conditions) {
+        vector<int> inDegree(k + 1, 0);
+        unordered_map<int, unordered_set<int>> graph;
+        
+        for (const auto& condition : conditions) {
+            int u = condition[0];
+            int v = condition[1];
+            if (graph[u].find(v) == graph[u].end()) {
+                graph[u].insert(v);
+                inDegree[v]++;
+            }
+        }
+        
+        queue<int> q;
+        for (int i = 1; i <= k; ++i) {
+            if (inDegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        
+        vector<int> order;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            order.push_back(node);
+            
+            for (int neighbor : graph[node]) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
+            }
+        }
+        
+        if (order.size() != k) {
+            return {};
+        }
+        
+        return order;
+    }
+};
