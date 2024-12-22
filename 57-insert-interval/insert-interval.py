46f89a2b6c8b276b69a6db51a6d9ec212a895c4e
UnnayanNITP <157901866+UnnayanNITP@users.noreply.github.com>
2024-12-22 12:49:30 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (5.63%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,16 +1,42 @@
-class Solution:
+class Solution(object):
     def insert(self, intervals, newInterval):
-        result = []
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        new_start = newInterval[0]
+        new_end = newInterval[1]
+
         i = 0
-        while i < len(intervals) and intervals[i][1] < newInterval[0]:
-            result.append(intervals[i])
-            i += 1
-        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(newInterval[0], intervals[i][0])
-            newInterval[1] = max(newInterval[1], intervals[i][1])
-            i += 1
-        result.append(newInterval)
-        while i < len(intervals):
-            result.append(intervals[i])
-            i += 1
-        return result
+        n = len(intervals)
+        output = []
+
+        # add all the intervals that come before the new interval
+        while i < n and new_start > intervals[i][0]:
+            # print(f"the output is {output}")
+            output.append(intervals[i])
+            # intervals_i = intervals[i]
+            # print(f"The intervals[i] is {intervals_i}")
+            i +=1
+            # print(f"Current output {output}")
+
+        # merge the newInterval with last interval if nessary
+        if not output or output[-1][1] < new_start:
+            output.append(newInterval)
+        else:
+            output[-1][1] = max(output[-1][1], new_end)
+
+        while i < n:
+            start, end = intervals[i]
+            # print(f"start: {start}")
+            # print(f"end: {end}")
+
+            if output[-1][1] < start:
+                output.append([start, end])
+            else:
+                output[-1][1] = max(output[-1][1], end)
+            i+=1
+
+        return output
+            
