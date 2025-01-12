9e8a5231546902c6897d326abd73968a5bc63e77
UnnayanNITP <157901866+UnnayanNITP@users.noreply.github.com>
2025-01-12 17:37:40 +0530

    Time: 138 ms (19.88%) | Memory: 18.7 MB (38.33%) - LeetSync

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canBeValid(self, s: str, locked: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+        
+        low, high = 0, 0
+        for i in range(len(s)):
+            if locked[i] == '1':
+                if s[i] == '(':
+                    low += 1
+                    high += 1
+                else:
+                    low -= 1
+                    high -= 1
+            else:
+                low -= 1
+                high += 1
+            
+            low = max(low, 0)
+            if high < 0:
+                return False
+        
+        return low == 0
