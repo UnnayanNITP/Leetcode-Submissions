8fb92dd7fbae2dd492ade2cd04353c7d58e0089d
UnnayanNITP <157901866+UnnayanNITP@users.noreply.github.com>
2025-02-01 18:57:37 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (25.00%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -1,24 +1,10 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
-        // nums = [2,1,4]
-        // if size = 1 return true
-        // if size > 1
-        // for i = 1 to size -2
-        // if (nums[i-1]%2==0!=nums[i+1]%2==0)
-        // return true
-        
-        if (nums.size()==1)
-        {
-            return true;
+        int n = nums.size();
+        for(int i=1;i<n; i++){
+            if (nums[i-1]%2 == nums[i]%2) return false;
         }
-        for (int i = 1;i<nums.size();i++)
-            {
-                if ((nums[i]%2==0)==(nums[i-1]%2==0))
-                {
-                   return false;
-                }
-            }
         return true;
     }
 };
